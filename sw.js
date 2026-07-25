51fca5cbe8d53c5d8cab9e8cede877d0c80ed77b
Moverfelt <mikeoverfelt@yahoo.com>
2026-07-25 14:13:44 -0500

    Boost cinematic mission sound (#8)
    * Boost cinematic mission sound
    
    * Add Vegas radar and field tools
    
    ---------
    
    Co-authored-by: Michael Overfelt <michaeloverfelt@Michaels-MacBook-Air.local>

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE = 'omc-v7.0.0';
+const CACHE = 'omc-v7.1.0';
 const CORE = [
   './',
   './index.html',
-  './styles.css?v=7.0.0',
-  './app.js?v=7.0.0',
+  './styles.css?v=7.1.0',
+  './app.js?v=7.1.0',
   './manifest.webmanifest',
   './icons/icon-180.png',
   './icons/icon-192.png',
